7442385aa4cba47100b9a8c042bb68b301bbfa3e
jturner <jpt75@cornell.edu>
2020-02-23 22:02:20 -0500

    starting with rcnn

---
+++ models/RCNN.py
@@ -0,0 +1,10 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas
+import tensorflow as tf
+
+ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+
+# TODO: everything
